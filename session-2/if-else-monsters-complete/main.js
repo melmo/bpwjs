2b22da7dac00db2c1975f63654c166f08dc72d0f
Melanie Thewlis <melaniethewlis@littlewebgiants.com>
2017-04-20 07:21:28 +0200

    session 2 complete

--- session-2/if-else-monsters-complete/main.js
+++ session-2/if-else-monsters-complete/main.js
@@ -1,14 +1,33 @@
 function monsters() {
-	var choice1 = prompt("You are walking in the forest, when a monster jumps out. Do you FIGHT, pay or run?","Type your answer here").toUpperCase();
+	var choice1 = prompt("You are walking in the forest, when a monster jumps out. Do you FIGHT, PAY or RUN?","Type your answer here").toUpperCase();
 
 	if (choice1 === "FIGHT") {
 		var choice2 = prompt("Are you strong?","Yes or No").toUpperCase();
 		if (choice2 === "YES") {
 			alert("You beat the monster!");
 		} else {
-			// What happens next?
+			var choice3 = prompt("Are you smart?","Yes or No").toUpperCase();
+			if(choice3 === "YES") {
+		      alert("I guess that'll do! You beat the monster - nice work!");
+		    } else {
+		      alert("You're not strong OR smart? Well, if you were smarter, you probably wouldn't have tried to fight a monster. You lose!");
+		    }
 		}
 
+	} else if (choice1 == "PAY") {
+		var choice2 = prompt("Are you rich?","Yes or No").toUpperCase();
+		if (choice2 === "YES") {
+			alert("Well I guess you can afford 100€!");
+		} else {
+			alert("Too bad! It's human for dinner!")
+		}
+	} else if (choice1 == "RUN") {
+		var choice2 = prompt("Are you fast?","Yes or No").toUpperCase();
+		if (choice2 === "YES") {
+			alert("Better get your running shoes on then!");
+		} else {
+			alert("This doesn't end well for you, tiny human!")
+		}
 	}
 }
 
