f7d2656fe5911263d5a1058ca300ea673cc126a5
Melanie Thewlis <melaniethewlis@littlewebgiants.com>
2017-04-20 07:33:56 +0200

    dom manipulation

---
+++ session-4/click-events/main.js
@@ -0,0 +1,8 @@
+function changeColor(color) {
+  var circle = document.getElementById('circle');
+	circle.style.backgroundColor = color;
+}
+
+function outputValue(elem) {
+  document.getElementById('output').innerHTML = elem.value;
+}
